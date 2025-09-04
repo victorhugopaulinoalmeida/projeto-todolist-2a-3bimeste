89c812adf6576b9f185f0892a3621b09a45a136b
Pedro Yuji Hamada <pedro.yuji.hamada@gmail.com>
2025-09-04 11:55:17 -0300

    {feat} Add tarefa

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import imgLogo from ' ./assets/icon_todo_list.png';
+ import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View, Alert } from "react-native";
+import logo from "./assets/logo.png";
+import icone from "./assets/icone.png";
+import { useState } from "react";
 
 export default function App() {
+
+  const [tarefa, setTarefa] = useState("");
+  const [tarefas, setTarefas] = useState([]);
+
+
+
+  
+
+const handleAdd = () => {
+ //Alert.alert(tarefa);
+ setTarefas([tarefa, ...tarefas]);
+  setTarefa("");
+}
+
   return (
     <View style={styles.container}>
-    <View>
-      <Image source={imgLogo} />
-    </View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Image source={logo} style={styles.logo} />
+        <Text>todo list</Text>
+      </View>
+      <View>
+        <TextInput
+          placeholder="Add Tarefa"
+          value={tarefa}
+          onChangeText={setTarefa}
+        />
+        <TouchableOpacity onPress={handleAdd}>
+        <Image source={icone} style={styles.icone} />
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,13 +43,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  logo:{
+  logo: {
     width: 128,
-    height: 128
+    height: 128,
+  },
+  botaoAdd: {
+    width: 32,
+    height: 32,
   }
- });
-
+});
